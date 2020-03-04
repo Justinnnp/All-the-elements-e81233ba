22dc6b29114ba1ad75ba5d38c4270ea4fe9d58d6
Justin Plein <2099449@talnet.nl>
2020-03-04 10:35:20 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// var paragraph = document.querySelectorAll("p");
-
-// paragraph[0].classList.add("italic"); 
-// paragraph[1].classList.add("italic");
-// paragraph[2].classList.add("italic");
-// paragraph[3].classList.add("italic");
-
-// var h1 = document.querySelectorAll("h1");
-
-// h1[0].classList.add("h1");
-
 var list;
 list = document.querySelectorAll("p");
 
